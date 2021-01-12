8b586fadf796c69c8ced22d775bd69a15694c877
Guest <guest-xaj3kw@h26817.sgb.skir.kr>
2021-01-12 23:55:19 +0900

    imcomplete

--- main.js
+++ main.js
@@ -7,3 +7,33 @@ const userDataList = [
     { id: 101, name: '호랑이' }
   ];
   
+function printList(){
+    const resultList = document.getElementById('resultList');
+    resultList.innerHTML ='';
+    for(const data of userDataList ){
+        resultList.innerHTML += `<li>${data.id} : ${data.name}</li>`;
+    }
+}
+
+function UpperSort(){
+    userDataList.sort((a,b) => {
+        return a.id -b.id;
+    })
+
+    printList();
+}
+
+function LowerSort(){
+    userDataList.sort((a,b) => {
+        return b.id - a.id;
+    })
+
+    printList();
+}
+
+const upperBtn = document.getElementById('upperSort');
+const lowerBtn = document.getElementById('lowerSort');
+
+upperBtn.addEventListener('click', UpperSort);
+lowerBtn.addEventListener('click', LowerSort);
+printList();
